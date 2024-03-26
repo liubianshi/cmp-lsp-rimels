ea5b07d50a9452be251030172cfcc4d145e374a8
liu.bian.shi <liu.bian.shi@gmail.com>
2024-03-26 19:02:35 +0800

    Add function to detect English environment in Norg

--- lua/rimels/english_environment_detectors.lua
+++ lua/rimels/english_environment_detectors.lua
@@ -1,5 +1,32 @@
 local M = {with_treesitter = {}, with_syntax = {}}
 
+function M.with_treesitter.norg(info)
+  info = info or vim.inspect_pos()
+  local trees = info.treesitter
+  local extmarks = info.extmarks
+  local englist_env = false
+  for _, ts in ipairs(trees) do
+    if
+      ts.capture == "neorg.markup.variable" or
+      ts.capture == "neorg.markup.verbatim" or
+      ts.capture == "neorg.markup.inline_math"
+    then
+      return true
+    elseif ts.capture == "comment" then
+      return false
+    end
+  end
+  for _,ext in ipairs(extmarks) do
+    if
+      ext.opts and
+      ext.opts.hl_group == "@neorg.tags.ranged_verbatim.code_block"
+    then
+      return true
+    end
+  end
+  return englist_env
+end
+
 function M.with_treesitter.markdown(info)
   info = info or vim.inspect_pos()
   local trees = info.treesitter
