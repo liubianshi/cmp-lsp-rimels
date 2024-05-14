69809295a63370759294234907ede85a553515bf
liu.bian.shi <liu.bian.shi@gmail.com>
2024-05-14 21:05:15 +0800

    Update root_dir to use a function instead of util pattern

--- lua/rimels/init.lua
+++ lua/rimels/init.lua
@@ -111,7 +111,7 @@ function M.setup(opts)
       default_config = {
         name = "rime_ls",
         cmd = opts.cmd,
-        root_dir = util.root_pattern('.root', '.svn', '.git', '.hg', '.project', '.obsidian'),
+        root_dir = function() end,
         filetypes = opts.filetypes,
         single_file_support = opts.single_file_support,
       },
