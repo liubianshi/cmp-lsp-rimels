d24c9b934da01d7896e4bd7dbb8997f0ade824d8
liu.bian.shi <liu.bian.shi@gmail.com>
2024-07-16 11:16:17 +0800

    Fix: 方案选择失效的问题 #5

--- lua/rimels/default_opts.lua
+++ lua/rimels/default_opts.lua
@@ -5,6 +5,8 @@ local M = {
   settings = {},
   rime_user_dir = "~/.local/share/rime-ls",
   shared_data_dir = "/usr/share/rime-data",
+  paging_characters = {",", ".", "-", "="},
+  trigger_characters = {},
   always_incomplete = false,
   docs = {
     description = [[https://www.github.com/wlh320/rime-ls, A language server for librime]],
@@ -16,7 +18,6 @@ local M = {
     undo = ";u",
   },
   max_candidates = 9,
-  trigger_characters = {},
   schema_trigger_character = "&", -- [since v0.2.0] 当输入此字符串时请求补全会触发 “方案选单”
   probes = {
     ignore = {},
