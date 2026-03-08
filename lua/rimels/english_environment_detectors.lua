e7fd45f41f3d31d18a9bea999dd86990ec0cf761
liu.bian.shi <liu.bian.shi@gmail.com>
2026-03-09 00:23:17 +0800

    Revert "feat(.claude/worktrees): add tender-humming-lantern worktree to support collaborative development workflows"
    This reverts commit 85ef2320afff64de61bc4b4e86b617f8864abc92.

--- lua/rimels/english_environment_detectors.lua
+++ lua/rimels/english_environment_detectors.lua
@@ -1,4 +1,4 @@
-local M = { with_treesitter = {}, with_syntax = {} }
+local M = {with_treesitter = {}, with_syntax = {}}
 
 function M.with_treesitter.norg(info)
   info = info or vim.inspect_pos()
@@ -7,19 +7,19 @@ function M.with_treesitter.norg(info)
   local englist_env = false
   for _, ts in ipairs(trees) do
     if
-      ts.capture == "neorg.markup.variable"
-      or ts.capture == "neorg.markup.verbatim"
-      or ts.capture == "neorg.markup.inline_math"
+      ts.capture == "neorg.markup.variable" or
+      ts.capture == "neorg.markup.verbatim" or
+      ts.capture == "neorg.markup.inline_math"
     then
       return true
     elseif ts.capture == "comment" then
       return false
     end
   end
-  for _, ext in ipairs(extmarks) do
+  for _,ext in ipairs(extmarks) do
     if
-      ext.opts
-      and ext.opts.hl_group == "@neorg.tags.ranged_verbatim.code_block"
+      ext.opts and
+      ext.opts.hl_group == "@neorg.tags.ranged_verbatim.code_block"
     then
       return true
     end
@@ -32,7 +32,10 @@ function M.with_treesitter.markdown(info)
   local trees = info.treesitter
   local englist_env = false
   for _, ts in ipairs(trees) do
-    if ts.capture == "markup.math" or ts.capture == "markup.raw" then
+    if
+      ts.capture == "markup.math" or
+      ts.capture == "markup.raw"
+    then
       return true
     elseif ts.capture == "markup.raw.block" then
       englist_env = true
@@ -51,10 +54,10 @@ function M.with_syntax.markdown(info)
     local hl = syn.hl_group
     local hl_link = syn.hl_group_link
     if
-      hl == "pandocLaTeXInlineMath"
-      or hl == "pandocNoFormatted"
-      or hl == "pandocOperator"
-      or hl == "pandocLaTeXMathBlock"
+      hl == "pandocLaTeXInlineMath" or
+      hl == "pandocNoFormatted" or
+      hl == "pandocOperator" or
+      hl == "pandocLaTeXMathBlock"
     then
       return true
     elseif hl == "pandocDelimitedCodeBlock" then
